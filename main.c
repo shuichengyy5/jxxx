b05186b909a305d801d7836d2f36bfded85a7af2
shuichengyy5 <3331592296@qq.com>
2024-11-09 22:44:00 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
 //202410111303
 //3331592296@ qq.com
 //余洋
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	int n;
-	int i;
-	double sum=1;
-	
-	printf("");
-	scanf("%d",&n);
-	
-	for(i=1;i<=n;i++){
-		sum*=i;
-	}
-	
-	printf("%f\n",sum);
-	
-	return 0;
+unsigned long long factorial(int n);
+
+int main() {
+    int number;
+    printf(" ");
+    scanf("%d", &number);
+
+    if (number < 0) {
+        printf("负数没有阶乘。\n");
+    } else {
+        unsigned long long result = factorial(number);
+        printf("%llu\n",result);
+    }
+
+    return 0;
+}
+
+unsigned long long factorial(int n) {
+    unsigned long long result = 1;  
+    for (int i = 1; i <= n; i++) {
+        result *= i;  
+    }
+    return result;  
 }
