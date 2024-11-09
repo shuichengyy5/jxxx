8c338703af776cf36dc96a8cd50c13d077b201a5
shuichengyy5 <3331592296@qq.com>
2024-11-09 22:34:12 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
-//
-//
-//
-#include <stdio.h>
-int main()
-{
-    printf("Hello World");
-    return 0;
+//202410111303
+//3331592296@ qq.com
+//余洋
+#include<stdio.h>
+
+int main(){
+	int n;
+	int i;
+	double sum=1;
+	
+	printf("");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++){
+		sum*=i;
+	}
+	
+	printf("%f\n",sum);
+	
+	return 0;
 }
